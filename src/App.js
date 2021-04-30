5afe4ea8a33fc3385f5d7f572aab113fe53ce852
eril_xpert <kejayaansebenar@gmail.com>
2021-04-30 16:12:08 +0800

    43. Exercise Where to Put State?

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component} from 'react';
-import {CardList} from './components/card-list/card-list.component'
+import {CardList} from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
       string : 'Hello Maher',
+      searchField : '',
       monsters : [
     
       ]
@@ -20,24 +22,27 @@ class App extends Component {
     // const saje = fetch('http://jsonplaceholder.typicode.com/users')
     // saje.then(response => response.json())
     // .then(users => this.setState({monsters:[{name:'Maher'}]}) )
-  }
+  }s
   render() {
+    const { searchField, monsters} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          
             Edit <code>src/App.js</code> and save to reload. 
-            have i succeeded?
+            {/* <input type='search' placeholder='Search Monster' onChange={e=>this.setState({searchField:e.target.value})}/> */}
+            <SearchBox placeholder='Search Monster' handleChange={e=>this.setState({searchField:e.target.value})}></SearchBox>
             <p>
-              <CardList cubaan='saje2' monsters={this.state.monsters}>
+              <CardList cubaan='saje2' monsters={filteredMonsters}>
                 {/* {this.state.monsters.map(monster => <p>{monster.name}</p>)} */}
               </CardList>
             {this.state.string}
             </p>
             <p>
             {/* {this.state.monsters.map(monster => <p>{monster.name}</p>)} */}
-            </p>
+            
 
             <button onClick ={() => this.setState({string:'Assalamualaikum itu lebih baik...'})}>
               Click Me
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,9 +5,9 @@ import './card.styles.css'
 export const Card = props => (
     
         <div className='card-container'>
-            <h2 key={props.monster.id}>
-                        {props.monster.name}
-                    </h2>
+            <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
+            <h2 >{props.monster.name}</h2>
+            <p>{props.monster.email}</p>
         </div>
             
 )
